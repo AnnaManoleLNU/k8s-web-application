b6f958a3f01a4b0501a3851e693ab5546fca5bd4
Anna Manole <am224wd@student.lnu.se>
2024-11-14 16:02:47 +0100

    rabbits?

--- src/server.js
+++ src/server.js
@@ -15,10 +15,14 @@ import { router } from './routes/router.js'
 import { randomUUID } from 'node:crypto'
 import { morganLogger } from './config/morgan.js'
 import { logger } from './config/winston.js'
+import { NotificationService } from './controllers/NotificationService.js'
 
 try {
   // Connect to MongoDB.
   await connectToDatabase(process.env.DB_CONNECTION_STRING)
+
+  // Instantiate the NotificationService
+const notificationService = new NotificationService();
 
   // Creates an Express application.
   const app = express()
